030aa2a0193de5ea7157137382c71861e914c12e
mtotodev05 <mtotodev05@gmail.com>
2018-12-09 16:26:49 +0300

    Setup graphql,endpoints working

--- app.js
+++ app.js
@@ -4,12 +4,45 @@ const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./api/routes/index');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 app.use('/',routes);
+
+app.use('/graphql',graphQlHttp({
+    schema:buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String 
+        }
+    
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue:{
+        events: ()=>{
+            return [
+                'Romantic' ,'Writing Code' ,'Sailing' , 'Code is Fun'
+            ];
+        },
+        createEvent: (args)=>{
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql:true
+}));
 
 app.use(function(req,res,next){
    const error = new Error('Route Not Found');
