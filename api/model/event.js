45be20d6fe145d5f17bc2ecafe58d9ff8c9503c5
mtotodev05 <mtotodev05@gmail.com>
2018-12-14 17:38:35 +0300

    v1, event Schema

---
+++ api/model/event.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+    _id:mongoose.Schema.Types.ObjectId,
+    title:{type:String,required:true},
+    description:{type:String,required:true},
+    price:{type:Number,required:true},
+    date:{type:Date,required:true}
+});
+
+module.exports = mongoose.model('Events',eventSchema);
