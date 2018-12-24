5266a67364fa0c96129df69b9fdeeb32a2e00bb4
mtotodev05 <mtotodev05@gmail.com>
2018-12-24 14:22:42 +0300

    Check for user email redundancies before commit

--- app.js
+++ app.js
@@ -110,28 +110,41 @@ app.use('/graphql',graphQlHttp({
                 })
         },
         createUser: (args)=>{
-           return bcyrpt.hash(args.userInput.password,12)
-                .then(hashedPassword=>{
-                    const user = new User({
-                        email: args.userInput.email,
-                        password: hashedPassword
-                    });
-                    return user.save()
-                        .then(result=>{
-                            return{
-                                ...result._doc,_id:result.id
-                            }
-                        })
-                        .catch(err=>{
-                            console.log(err.message);
-                            throw err;
-                        });
-                })
-                .catch(err=>{
-                    console.log(err.message);
-                    throw err;
+           return User.findOne({email:args.userInput.email})
+               .then(user=>{
+                    if(user){
+                        throw new Error('User Already Exists')
+                    }else{
+                        return bcyrpt.hash(args.userInput.password,12)
+                            .then(hashedPassword=>{
+                                const user = new User({
+                                    email: args.userInput.email,
+                                    password: hashedPassword
+                                });
+                                return user.save()
+                                    .then(result=>{
+                                        return{
+                                            ...result._doc,_id:result.id,
+                                            password:null
+                                        }
+                                    })
+                                    .catch(err=>{
+                                        console.log(err.message);
+                                        throw err;
+                                    });
+                            })
+                            .catch(err=>{
+                                console.log(err.message);
+                                throw err;
 
-                });
+                            });
+                    }
+               })
+               .catch(err=>{
+                   console.log('Error Fetching Users',err.message);
+                   throw err;
+               })
+
         }
     },
     graphiql:true
