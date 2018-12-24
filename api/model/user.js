831eb6510dea49902eef580622432e2d74acda01
mtotodev05 <mtotodev05@gmail.com>
2018-12-24 13:13:22 +0300

    Adding Relations

--- api/model/user.js
+++ api/model/user.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    createdEvents:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Event'
+        }
+    ]
 });
 
 module.exports = mongoose.model('User',userSchema);
