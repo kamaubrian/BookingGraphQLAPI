31ff9373309cbdd3ce49ae9fe0b588125bc4b801
mtotodev05 <mtotodev05@gmail.com>
2019-01-20 13:23:18 +0300

    Update .env

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(isAuth);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})
+
 app.use('/', routes);
 
 
